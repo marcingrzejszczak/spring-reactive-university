ecc54cc23e168e3273043126d51c035af9d0fc3c
Brian Clozel <bclozel@pivotal.io>
2016-11-06 10:24:18 +0100

    Call a remote REST API with the WebClient
    In this step, we're using the reactive HTTP `WebClient` to call a remote
    REST API. To do that, we first need to create an instance of that client
    and configure it if filter functions (which in our case, will add HTTP
    headers to the outgoing request).
    
    We can then compose two REST calls into a single Publisher of
    `ReactorIssue`s.
    
    Note that because the templating engine we're using here does not
    support `Publisher` types, we have to asynchronously resolve the whole
    model before triggering the template rendering phase.

--- src/main/java/com/example/integration/github/GithubClient.java
+++ src/main/java/com/example/integration/github/GithubClient.java
@@ -1,8 +1,15 @@
 package com.example.integration.github;
 
+import com.example.DashboardProperties;
 import reactor.core.publisher.Flux;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
+import org.springframework.web.reactive.function.client.WebClient;
 
 /**
  * @author Brian Clozel
@@ -10,9 +17,38 @@ import org.springframework.stereotype.Component;
 @Component
 public class GithubClient {
 
-	public Flux<GithubIssue> findOpenIssues(String owner, String repo) {
+	private static final MediaType VND_GITHUB_V3 = MediaType.valueOf("application/vnd.github.v3+json");
 
-		return Flux.empty();
+	private final WebClient webClient;
+
+	public GithubClient(DashboardProperties properties) {
+		this.webClient = WebClient
+				.builder(new ReactorClientHttpConnector())
+				.filter(ExchangeFilterFunctions
+						.basicAuthentication(properties.getGithub().getUsername(),
+								properties.getGithub().getToken()))
+				.filter(userAgent())
+				.build();
+	}
+
+	public Flux<GithubIssue> findOpenIssues(String owner, String repo) {
+		ClientRequest request = ClientRequest
+				.GET("https://api.github.com/repos/{owner}/{repo}/issues?state=open", owner, repo)
+				.accept(VND_GITHUB_V3)
+				.build();
+
+		return this.webClient.exchange(request).flatMap(response -> response.bodyToFlux(GithubIssue.class));
+	}
+
+
+	private ExchangeFilterFunction userAgent() {
+		return (clientRequest, exchangeFunction) -> {
+			ClientRequest newRequest = ClientRequest
+					.from(clientRequest)
+					.header("User-Agent", "Spring Framework WebClient")
+					.build();
+			return exchangeFunction.exchange(newRequest);
+		};
 	}
 
 }
